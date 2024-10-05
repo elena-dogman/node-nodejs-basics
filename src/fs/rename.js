a0ee49062ca0bc85ba70725d2d6c584d00f61ead
elena-dogman <dogman.choli@gmail.com>
2024-10-05 17:04:24 +0300

    feat: implement renaming

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,34 @@
-const rename = async () => {
-    // Write your code here 
-};
+import { promises as fs } from 'fs'
+import { join } from 'path'
 
-await rename();
+const rename = async () => {
+  const wrongFilename = join(
+    process.cwd(),
+    'src',
+    'fs',
+    'files',
+    'wrongFilename.txt'
+  )
+  const properFilename = join(
+    process.cwd(),
+    'src',
+    'fs',
+    'files',
+    'properFilename.md'
+  )
+
+  try {
+    await fs.access(wrongFilename)
+
+    await fs.access(properFilename)
+    throw new Error('FS operation failed')
+  } catch (error) {
+    if (error.code === 'ENOENT' && error.path === properFilename) {
+      await fs.rename(wrongFilename, properFilename)
+    } else {
+      throw new Error('FS operation failed')
+    }
+  }
+}
+
+await rename()
