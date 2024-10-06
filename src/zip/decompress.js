661d2772301caa4e7a0fb4617d4fc6437df49489
elena-dogman <dogman.choli@gmail.com>
2024-10-06 11:06:23 +0300

    feat: implement decompressing

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,33 @@
-const decompress = async () => {
-    // Write your code here 
-};
+import { createReadStream, createWriteStream } from 'fs'
+import { join } from 'path'
+import { createGunzip } from 'zlib'
 
-await decompress();
+const decompress = async () => {
+  const archivePath = join(process.cwd(), 'src', 'zip', 'files', 'archive.gz')
+  const decompressedFilePath = join(
+    process.cwd(),
+    'src',
+    'zip',
+    'files',
+    'fileToCompress.txt'
+  )
+
+  const readStream = createReadStream(archivePath)
+  const writeStream = createWriteStream(decompressedFilePath)
+  const gunzipStream = createGunzip()
+
+  readStream.pipe(gunzipStream).pipe(writeStream)
+
+  readStream.on('error', err =>
+    console.error(`Error reading archive: ${err.message}`)
+  )
+  writeStream.on('error', err =>
+    console.error(`Error writing file: ${err.message}`)
+  )
+
+  writeStream.on('finish', () => {
+    console.log('File successfully decompressed to fileToCompress.txt')
+  })
+}
+
+await decompress()
