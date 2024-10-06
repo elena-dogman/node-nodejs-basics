1db6781404c4f6eed429a97cbe1b9337491dfa2a
elena-dogman <dogman.choli@gmail.com>
2024-10-06 10:48:50 +0300

    feat: implement calculating hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,32 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import { createHash } from 'crypto'
+import { createReadStream } from 'fs'
+import { join } from 'path'
 
-await calculateHash();
+const calculateHash = async () => {
+  const filePath = join(
+    process.cwd(),
+    'src',
+    'hash',
+    'files',
+    'fileToCalculateHashFor.txt'
+  )
+
+  const stream = createReadStream(filePath)
+
+  const hash = createHash('sha256')
+
+  stream.on('data', chunk => {
+    hash.update(chunk)
+  })
+
+  stream.on('end', () => {
+    const hexHash = hash.digest('hex')
+    console.log(`SHA256 hash: ${hexHash}`)
+  })
+
+  stream.on('error', err => {
+    console.error(`Error reading file: ${err.message}`)
+  })
+}
+
+await calculateHash()
