34edd9a3a8a1647c3a8f3f7bb4de08f52bd8a9b0
elena-dogman <dogman.choli@gmail.com>
2024-10-06 10:56:02 +0300

    feat: implement writable stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,26 @@
-const write = async () => {
-    // Write your code here 
-};
+import { createWriteStream } from 'fs'
+import { join } from 'path'
 
-await write();
+const write = async () => {
+  const filePath = join(
+    process.cwd(),
+    'src',
+    'streams',
+    'files',
+    'fileToWrite.txt'
+  )
+
+  const writableStream = createWriteStream(filePath)
+
+  process.stdin.pipe(writableStream)
+
+  writableStream.on('error', err => {
+    console.error(`Error writing to file: ${err.message}`)
+  })
+
+  writableStream.on('finish', () => {
+    console.log('Finished writing to file')
+  })
+}
+
+await write()
